7a6f4ccaccf80afb15dd343901a38103224f25b1
Masahiro Harada <poe.422@gmail.com>
2020-06-06 14:09:17 +0900

    add missing piece / remove needless setting

--- generators/app/templates/webpack.mix.js
+++ generators/app/templates/webpack.mix.js
@@ -4,6 +4,5 @@ mix
   .setPublicPath('dist/')
   .js('src/js/app.js', 'dist/js')
   .sass('src/sass/app.scss', 'dist/css')
-  .sass('src/sass/docs.scss', 'dist/css')
   .copy('src/images', 'dist/images')
 ;
